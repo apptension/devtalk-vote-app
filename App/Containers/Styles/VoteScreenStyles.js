5abe9088586dd4f6ccc7d26627d0a13746a93e08
Marcin Bazanowski <mbazanowski@apptension.com>
2017-10-10 20:06:55 +0200

    add vote buttons + handle clicks

--- App/Containers/Styles/VoteScreenStyles.js
+++ App/Containers/Styles/VoteScreenStyles.js
@@ -9,4 +9,20 @@ export default StyleSheet.create({
   centered: {
     alignItems: 'center',
   },
+  voteContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'stretch',
+    alignContent: 'stretch',
+    flexGrow: 4,
+    height: 300
+  },
+  voteButton: {
+    flexBasis: '49%',
+    flexShrink: 1,
+    flexGrow: 1,
+    justifyContent: 'center',
+    backgroundColor: 'red',
+    alignItems: 'center',
+  }
 });
--- App/Containers/VoteScreen.js
+++ App/Containers/VoteScreen.js
@@ -1,13 +1,21 @@
 import React, { PropTypes, Component } from 'react';
-import { ScrollView, Text, View, Button } from 'react-native';
+import { ScrollView, Text, View, Button, TouchableHighlight } from 'react-native';
+import { connect } from 'react-redux';
 
 // Styles
 import styles from './Styles/VoteScreenStyles';
 import colors from '../Themes/Colors';
 
-export default class VoteScreen extends Component {
+export class VoteScreenComponent extends Component {
   static propTypes = {
     navigation: PropTypes.object.isRequired,
+    onVote: PropTypes.func.isRequired,
+  };
+
+  handleVoteClick = (value) => {
+    const { onVote } = this.props;
+    console.log(value);
+    onVote(value);
   };
 
   render() {
@@ -21,6 +29,28 @@ export default class VoteScreen extends Component {
               Vote Screen
             </Text>
           </View>
+          <View style={styles.voteContainer}>
+            <TouchableHighlight style={styles.voteButton} onPress={() => this.handleVoteClick(1)} underlayColor="orange">
+              <View>
+                <Text>1</Text>
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.voteButton} onPress={() => this.handleVoteClick(2)} underlayColor="orange">
+              <View>
+                <Text>2</Text>
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.voteButton} onPress={() => this.handleVoteClick(3)} underlayColor="orange">
+              <View>
+                <Text>3</Text>
+              </View>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.voteButton} onPress={() => this.handleVoteClick(4)} underlayColor="orange">
+              <View>
+                <Text>4</Text>
+              </View>
+            </TouchableHighlight>
+          </View>
           <View style={styles.section}>
             <Button title="Back" color={colors.green} onPress={() => navigation.goBack()} />
           </View>
@@ -29,3 +59,11 @@ export default class VoteScreen extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = (dispatch) => ({
+  onVote: (value) => {alert(`Zaglosowales: ${value}`);}
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(VoteScreenComponent);

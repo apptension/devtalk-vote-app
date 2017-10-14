5ab07882ffa9aa5d9ace73929544fc3385e9c0c3
Marcin Bazanowski <mbazanowski@apptension.com>
2017-10-14 15:23:42 +0200

    add vote screen redux

---
+++ App/Redux/VoteScreenRedux.js
@@ -0,0 +1,33 @@
+import { createActions, createReducer } from 'reduxsauce';
+import Immutable from 'seamless-immutable';
+
+/* ------------- Types and Action Creators ------------- */
+
+export const POLL_STATUS_NOT_STARTED = 'not-started';
+export const POLL_STATUS_STARTED = 'started';
+export const POLL_STATUS_RESULTS = 'results';
+
+export const VOTE_STATUS_NOT_VOTED = 'not-voted';
+export const VOTE_STATUS_VOTED = 'voted';
+
+export const { Types: VoteScreenTypes, Creators: VoteScreenActions } = createActions({
+  fetchPoll: [],
+  fetchPollSuccess: ['data'],
+});
+
+const INITIAL_STATE = Immutable({
+  poll: {
+    status: VOTING_STATUS_NOT_STARTED,
+    name: '',
+  },
+  vote: {
+    status: VOTE_STATUS_NOT_VOTED,
+    value: null,
+  }
+});
+
+const fetchPollSuccessHandler = (state, { data }) => state.set('poll', Immutable(data));
+
+export const reducer = createReducer(INITIAL_STATE, {
+  [VoteScreenTypes.FETCH_POLL_SUCCESS]: fetchPollSuccessHandler,
+});

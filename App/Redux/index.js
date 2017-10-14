0b8c136dba948828032e83e0770676269f7adedd
Marcin Bazanowski <mbazanowski@apptension.com>
2017-10-14 19:26:03 +0200

    add Voting reducer

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -2,12 +2,18 @@ import { combineReducers } from 'redux';
 import configureStore from './CreateStore';
 import rootSaga from '../Sagas/';
 
+import {reducer as navReducer} from './NavigationRedux';
+import {reducer as userAuthReducer} from './UserAuthRedux';
+import {reducer as launchScreenReducer} from './AdminListRedux';
+import {reducer as votingReducer} from "./VotingRedux"
+
 export default () => {
   /* ------------- Assemble The Reducers ------------- */
   const rootReducer = combineReducers({
-    nav: require('./NavigationRedux').reducer,
-    userAuth: require('./UserAuthRedux').reducer,
-    launchScreen: require('./AdminListRedux').reducer,
+    nav: navReducer,
+    userAuth: userAuthReducer,
+    launchScreen: launchScreenReducer,
+    voting: votingReducer
   });
 
   return configureStore(rootReducer, rootSaga);

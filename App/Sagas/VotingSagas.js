5d2e75868d1bc29409c313bcb8eb2a4b375997f6
Marcin Bazanowski <mbazanowski@apptension.com>
2017-10-15 14:16:52 +0200

    add close poll saga with mocked results

--- App/Sagas/VotingSagas.js
+++ App/Sagas/VotingSagas.js
@@ -5,7 +5,22 @@ import { VotingActions, VotingTypes } from '../Redux/VotingRedux';
 
 export function* sendVote(value) {
   try {
-      yield put(VotingActions.sendVoteSuccess(value));
+    yield put(VotingActions.sendVoteSuccess(value));
+  } catch (error) {
+    console.error(error); // eslint-disable-line
+  }
+}
+
+export function* closePoll() {
+  try {
+    const results = {
+      ['1']: 2,
+      ['2']: 4,
+      ['3']: 8,
+      ['4']: 2,
+    };
+    const total = Object.keys(results).reduce((sum, point) => (sum += point*results[point]), 0);
+    yield put(VotingActions.closePollSuccess({results, total}));
   } catch (error) {
     console.error(error); // eslint-disable-line
   }
@@ -14,6 +29,7 @@ export function* sendVote(value) {
 export function* votingSaga() {
   yield all([
     takeLatest(VotingTypes.SEND_VOTE, sendVote),
+    takeLatest(VotingTypes.CLOSE_POLL, closePoll),
   ]);
 }
 
